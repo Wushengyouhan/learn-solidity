81151213df20d3c809a6dbff0083f911e556f72f
Wushengyouhan <840835067@qq.com>
2025-02-18 11:53:43 +0800

    44. Testing with Mocha

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,3 +4,26 @@ const { Web3 } = require("web3");
 const web3 = new Web3(ganache.provider());
 
 // contract test code will go here
+class Car {
+  park() {
+    return "stopped";
+  }
+  drive() {
+    return "vroom";
+  }
+}
+
+let car;
+
+beforeEach(() => {
+  car = new Car();
+});
+
+describe("Car", () => {
+  it("can park", () => {
+    assert.equal(car.park(), "stopped");
+  });
+  it("can drive", () => {
+    assert.equal(car.drive(), "vroom");
+  });
+});
